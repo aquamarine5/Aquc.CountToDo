fbdf2eda48e5b4b71a6fc6b8030b042810829ba9
aquamarine5 <50990424+aquamarine5@users.noreply.github.com>
2023-07-29 17:36:24 +0800

    init

---
+++ Aquc.CountToDo.Service/Program.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Aquc.CountToDo.Service
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            /*
+            using IHost host = new HostBuilder()
+                .UseWindowsService(option =>
+                {
+                    option.ServiceName = "Aquc.CountToDo";
+                })
+                .ConfigureLogging(builder =>
+                {
+                    builder.ClearProviders();
+                    builder.AddConsole();
+                    builder.AddFile();
+                })
+                .ConfigureServices(container =>
+                {
+                    container.AddHostedService<Service>();
+                })
+                .Build();
+            */
+            var i = new Stackbricks.Interop.StackbricksInterop(new FileInfo("Aquc.Stackbricks.exe"));
+            var r = await i.Update();
+            if (r.IsSuccess)
+            {
+
+                Console.WriteLine(r.Value!.version);
+                Console.WriteLine(r.Value!.needUpdate);
+            }
+            else
+            {
+                Console.WriteLine(r.Exception!.message);
+                Console.WriteLine(r.Exception!.type);
+            }
+        }
+
+    }
+}
---
+++ Aquc.CountToDo.Service/Service.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aquc.CountToDo.Service
+{
+    partial class Service : BackgroundService
+    {
+        public Service()
+        {
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
